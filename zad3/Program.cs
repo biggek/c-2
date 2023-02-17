4087bc5c6d17e350cd3baf63012cbe696ca9dee6
Sergey <lolito0700@mail.ru>
2023-02-17 13:07:00 +0300

    zad3

---
+++ zad3/Program.cs
@@ -0,0 +1,7 @@
+﻿
+      Console.WriteLine ("Введите день недели...");
+      int dayofweek = int.Parse (Console.ReadLine());
+      if (dayofweek == 6 || dayofweek == 7) Console.WriteLine ("Ответ: Выходной день.");
+      else if (dayofweek <= 5) Console.WriteLine ("Ответ: Будний день.");
+      else if (dayofweek > 7) Console.WriteLine ("Ошибка: В неделе всего 7 дней!");
+		
