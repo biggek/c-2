5e4aa756c01692a3f4a8a5bf8250e1b9a3830e18
Sergey <lolito0700@mail.ru>
2023-02-17 12:57:14 +0300

    za21

---
+++ zad2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int ThirdDigit(int number)
+        {
+            int result = -1;
+            if (number >= 100)
+            {
+                while (number > 999)
+                {
+                    number = number / 10;
+                }
+                result = number % 10;
+            }
+            return result; 
+        }       
+if (ThirdDigit(number1) == -1)
+Console.WriteLine("третьей цифры нет");
+else
+Console.WriteLine($"Третье число {ThirdDigit(number1)}");
